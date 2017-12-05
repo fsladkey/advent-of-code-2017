f6704815fe76766a0ad5933603f25885f43ec1c9
Fred Sladkey <fsladkey@gmail.com>
2017-12-05 11:02:03 -0500

    Add day 3

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -1,89 +1,9 @@
-### Day 1
+package main
 
-```go
-func getNextIdx(input string, idx int) int {
-    return (idx + 1) % len(input)
-}
+import (
+	"fmt"
+)
 
-func getCircleIdx(input string, idx int) int {
-    return (idx + len(input)/2) % len(input)
-}
-
-func inverseCaptcha(input string, getNextIdx func(input string, idx int) int) int {
-    count := 0
-    for idx := range input {
-        if input[idx] == input[getNextIdx(input, idx)] {
-            char := string(input[idx])
-            num, err := strconv.Atoi(char)
-            if err != nil {
-                panic(err)
-            }
-            count += num
-        }
-    }
-    return count
-}
-
-func inverseCaptchaPt1(input string) int {
-    return inverseCaptcha(input, getNextIdx)
-}
-
-func inverseCaptchaPt2(input string) int {
-    return inverseCaptcha(input, getCircleIdx)
-}
-```
-
-### Day 2
-```go
-func minMaxDifference(row []string) int {
-    firstVal := strToInt(row[0])
-    largest := firstVal
-    smallest := firstVal
-    for _, char := range row {
-        num := strToInt(char)
-        if num > largest {
-            largest = num
-        } else if num < smallest {
-            smallest = num
-        }
-    }
-    return largest - smallest
-}
-
-func evenlyDivisibleDifference(row []string) int {
-    for idx := 0; idx < len(row); idx++ {
-        num1 := strToInt(row[idx])
-        for idy := 1; idy < len(row); idy++ {
-            num2 := strToInt(row[(idx+idy)%len(row)])
-            if num1%num2 == 0 {
-                return num1 / num2
-            }
-        }
-    }
-    return 0
-}
-
-func corruptionChecksum(grid []string, getSumOfDifference func(row []string) int) int {
-    sumOfDifferences := 0
-    for _, line := range grid {
-        row := strings.Fields(line)
-        sumOfDifferences += getSumOfDifference(row)
-    }
-    return sumOfDifferences
-}
-
-func corruptionChecksum1(grid []string) int {
-    return corruptionChecksum(grid, minMaxDifference)
-}
-
-func corruptionChecksum2(grid []string) int {
-    return corruptionChecksum(grid, evenlyDivisibleDifference)
-}
-```
-
-### Day 3
-```go
-// ew
 type vector struct {
 	x int
 	y int
@@ -211,4 +131,8 @@ func spiralMemory2(input int) int {
 	val := getSpiralValueAboveMax(input)
 	return val
 }
-```
+
+func main() {
+	fmt.Println(spiralMemory(361527))
+	fmt.Println(spiralMemory2(361527))
+}
